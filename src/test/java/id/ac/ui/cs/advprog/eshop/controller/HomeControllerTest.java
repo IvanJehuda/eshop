ddf981eacf60b4561d240285547855ecc78610d1
IvanJehuda <ivanjangi@gmail.com>
2025-02-20 10:59:00 +0700

    [FEAT] Adding HomeControllerTest.java

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(SpringExtension.class)
+class HomeControllerTest {
+
+    @Test
+    void testHomePage() throws Exception {
+        HomeController homeController = new HomeController();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())  // Ensures HTTP 200 response
+                .andExpect(view().name("home"));  // Ensures "home" is returned
+    }
+}
