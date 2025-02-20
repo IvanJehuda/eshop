8cda54f71a0d3287f031091f88c62dafdb6be8cb
IvanJehuda <ivanjangi@gmail.com>
2025-02-20 14:09:24 +0700

    [FEAT] Adding mainMethodRunsSpringApplication to EshopApplicationTests.java

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,25 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class EshopApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(true).isTrue();
 	}
-
+	@Test
+	void mainMethodRunsSpringApplication() {
+		try (MockedStatic<SpringApplication> mocked = Mockito.mockStatic(SpringApplication.class)) {
+			EshopApplication.main(new String[]{});
+			mocked.verify(() -> SpringApplication.run(EshopApplication.class, new String[]{}));
+		}
+	}
 }
