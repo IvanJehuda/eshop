79b4c1b137add157f368ec6c19ae9dfc67a280fe
IvanJehuda <ivanjangi@gmail.com>
2025-02-28 18:30:00 +0700

    [FEAT] Splitting the CarService into Read and Write to implement SOLID Principle

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -1,58 +1,63 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
 import id.ac.ui.cs.advprog.eshop.model.Car;
-
-import id.ac.ui.cs.advprog.eshop.service.CarService;
+import id.ac.ui.cs.advprog.eshop.service.CarReadService;
+import id.ac.ui.cs.advprog.eshop.service.CarWriteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/car")
 class CarController {
+
+    private final CarReadService carReadService;
+    private final CarWriteService carWriteService;
+
     @Autowired
-    private CarService carService;
+    public CarController(CarReadService carReadService, CarWriteService carWriteService) {
+        this.carReadService = carReadService;
+        this.carWriteService = carWriteService;
+    }
 
     @GetMapping("/createCar")
-    public String createCarPage (Model model) {
-        Car car = new Car();
-        model.addAttribute("car", car);
+    public String createCarPage(Model model) {
+        model.addAttribute("car", new Car());
         return "CreateCar";
     }
 
     @PostMapping("/createCar")
-    public String createCarPost (@ModelAttribute Car car, Model model) {
-        carService.create(car);
+    public String createCarPost(@ModelAttribute Car car) {
+        carWriteService.create(car);
         return "redirect:listCar";
     }
 
     @GetMapping("/listCar")
-    public String carListPage (Model model) {
-        List<Car> allCars = carService.findAll();
+    public String carListPage(Model model) {
+        List<Car> allCars = carReadService.findAll();
         model.addAttribute("cars", allCars);
         return "CarList";
     }
 
     @GetMapping("/editCar/{carId}")
-    public String editCarPage (@PathVariable String carId, Model model) {
-        Car car = carService.findById(carId);
+    public String editCarPage(@PathVariable String carId, Model model) {
+        Car car = carReadService.findById(carId);
         model.addAttribute("car", car);
         return "EditCar";
     }
 
     @PostMapping("/editCar")
-    public String editCarPost (@ModelAttribute Car car, Model model) {
-        carService.update(car.getCarId(), car);
+    public String editCarPost(@ModelAttribute Car car) {
+        carWriteService.update(car.getCarId(), car);
         return "redirect:listCar";
     }
 
     @PostMapping("/deleteCar")
-    public String deleteCar (@RequestParam("carId") String carId) {
-        carService.deleteCarById(carId);
+    public String deleteCar(@RequestParam("carId") String carId) {
+        carWriteService.deleteCarById(carId);
         return "redirect:listCar";
     }
 }
