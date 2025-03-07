26cc9889dcff79625b6bbbb9e89ffec807512036
IvanJehuda <ivanjangi@gmail.com>
2025-03-07 15:59:35 +0700

    [RED] Adding Product Repository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,28 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import java.util.List;
+import java.util.Map;
+
+public class PaymentRepository {
+
+    public Payment save(Payment payment) {
+        return null;
+    }
+
+    public Payment findById(String paymentId) {
+        return null;
+    }
+
+    public List<Payment> findAll() {
+        return null;
+    }
+
+    public Payment addPayment(String orderId, String method, Map<String, String> paymentData) {
+        return null;
+    }
+
+    public Payment setStatus(String paymentId, String status) {
+        return null;
+    }
+}
